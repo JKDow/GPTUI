568e6d44c7af470efdb54cea311ed99167c73be3
JKDow <114895228+JKDow@users.noreply.github.com>
2023-07-10 19:56:21 +1000

    setup reading api key

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
-fn main() {
-    println!("Hello, world!");
+use std::env;
+use dotenv::dotenv;
+
+#[tokio::main]
+async fn main() {
+    let key = get_key();
+    println!("{key}");
+}
+
+fn get_key() -> String{
+    dotenv().ok(); 
+    env::var("OPENAI_API_KEY").expect("OPENAI_API_KEY must be set")
 }
