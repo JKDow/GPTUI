8e66a515057b40d2b41ce0bf3b8ac1206884e428
JKDow <114895228+JKDow@users.noreply.github.com>
2023-07-14 18:50:24 +1000

    Change main code to present chat CLI

--- src/main.rs
+++ src/main.rs
@@ -4,8 +4,8 @@ use gpt_cli::open_ai::{chat::Chat, objects::Model};
 async fn main() {
     // get message from user 
     println!("Welcome to gpt CLI");
-    //let mut chat = Chat::new(Model::Gpt3Turbo, true);
-    let mut chat = Chat::load_json("gpt-3.5-turbo_2023-07-14_18-41-17.json").unwrap();
-    chat.print_messages();
+    let mut chat = Chat::new(Model::Gpt3Turbo, true);
+    //let mut chat = Chat::load_json("gpt-3.5-turbo_2023-07-14_18-41-17.json").unwrap();
+    //chat.print_messages();
     chat.basic_loop().await;
 }
